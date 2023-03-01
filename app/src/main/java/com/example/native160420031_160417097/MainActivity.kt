7487de5b4364066201e8dc000c8853c76eeaf503
zenocrife <jasonredkiller@gmail.com>
2023-03-01 10:24:07 +0700

    Init

---
+++ app/src/main/java/com/example/native160420031_160417097/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.native160420031_160417097
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
